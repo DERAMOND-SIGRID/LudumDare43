b8b0005cde22ef388bb22526db62249be1cfbee9
DIANE-ATHENA <insomnia.diane@hotmail.fr>
2018-12-01 11:33:16 +0100

    Creation of the script PlayerMoves, attached to the player

---
+++ LudumDare43/Assets/Sripts/PlayerMoves.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoves : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKey("z"))
+        { gameObject.GetComponent<Transform>().Translate(Vector3.up);
+        }
+	}
+}
