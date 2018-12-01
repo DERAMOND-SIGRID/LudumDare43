6937f0fda1cd2d3a242a9c9ec9a1863e01b42428
DIANE-ATHENA <insomnia.diane@hotmail.fr>
2018-12-01 12:47:42 +0100

    Creation of the enemy's moves

---
+++ LudumDare43/Assets/Sripts/EnnemyMoves.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyMoves : MonoBehaviour {
+
+    private Vector3 playerPosition;
+
+    private float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+        moveSpeed = gameObject.GetComponent<CharacterData>().GetMoveSpeed();
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        playerPosition = GameObject.Find("Player").GetComponent<Transform>().position;
+
+        gameObject.GetComponent<Transform>().position = Vector3.MoveTowards(gameObject.GetComponent<Transform>().position, playerPosition, moveSpeed);
+	}
+}
